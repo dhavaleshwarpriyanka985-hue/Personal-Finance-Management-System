f40533b3776633923486ec51389072c4f52b0f38
priy <priyadhavaleshwar9739@gmail.com>
2026-08-08 11:59:10 +0530

    Connect database using environment variables

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "MySQL@123"
-DB_NAME = "finance_db"
-DB_PORT = 3307
-SECRET_KEY = "your_secret_key"
+import os
+
+DB_HOST = os.environ.get("MYSQLHOST", "localhost")
+DB_USER = os.environ.get("MYSQLUSER", "root")
+DB_PASSWORD = os.environ.get("MYSQLPASSWORD", "MySQL@123")
+DB_NAME = os.environ.get("MYSQLDATABASE", "finance_db")
+DB_PORT = int(os.environ.get("MYSQLPORT", 3307))
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "your_secret_key")
