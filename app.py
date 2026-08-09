bd080ad784d737bc8a3b7ed1d795b14a4e22e385
priy <priyadhavaleshwar9739@gmail.com>
2026-08-09 11:07:54 +0530

    Add home page route

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-from flask import Flask
+from flask import Flask, redirect
 from routes.report import report_bp
 
 app = Flask(__name__)
+
 app.register_blueprint(report_bp)
-# Secret Key
+
 app.secret_key = "your_secret_key"
 
-# Import all routes
 from routes.auth import *
 from routes.finance import *
 from routes.ai import *
 from routes.report import *
+
+
+@app.route("/")
+def home():
+    return redirect("/login")
 
 
 if __name__ == "__main__":
